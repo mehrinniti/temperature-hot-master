eb0027077b7a35b2bbd6f1797f2ee1f04d5e5f2c
mehrinniti <mehrinniti@gmail.com>
2021-09-06 00:41:05 +0600

    wether map

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const API_KEY = `d3a6993335db36c4055fe45320902eba`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    //  set wether icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+    // console.log(temperature);
+}
